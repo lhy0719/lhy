1f913b98b3b9db37bbcf23bd3421e0d92c083ecb
xuhaitao <xuhaitao@jd.com>
2017-12-22 12:03:07 +0800

    Initial commit

---
+++ src/main/java/com/lhy/lhy/LhyApplication.java
@@ -0,0 +1,12 @@
+package com.lhy.lhy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LhyApplication.class, args);
+	}
+}
